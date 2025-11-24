bcb0aa5d63477397b4b45f45a6199986a2d18724
Luiza Andrade <luiza.c.andrade@gmail.com>
2025-11-24 01:37:30 -0600

    global inputs

--- main.R
+++ main.R
@@ -3,108 +3,123 @@
 #                         Template main script                                 #
 #==============================================================================#
 
-# Select scripts to run (set section to TRUE to run it) ------------------------
 
-  IMPORT     <- FALSE
-  DEIDENTIFY <- FALSE
-  CLEAN      <- FALSE
-  TIDY       <- FALSE
-  CONSTRUCT  <- FALSE
-  ANALYZE    <- FALSE
+pacman::p_load(
+  tidyverse,
+  here,
+  janitor,
+  zoo
+)
 
-# Required packages ------------------------------------------------------------
+# Global inputs ----------------------------------------------------------------
 
-  library(here)
+global_parameters <-
+  read_csv(
+    here(
+      "data",
+      "global",
+      "parameters.csv"
+    )
+  ) %>% 
+  select(value, name) %>%
+  pivot_wider
 
-# File paths -------------------------------------------------------------------
-# For the file paths below to work, make sure to create a .Rprofile file in the
-# repository's root folder following the instructions on 
-# 
-  
-  CODE     <- here("code")
-  DATA_BOX <- here(Sys.getenv("BOX"), "project-folder", "data") 
-  DATA_GIT <- here("data")
-  OTUPUT   <- here("output")
-  DOC_BOX  <- here(Sys.getenv("BOX"), "project-folder", "documentation") 
-  DOC_GIT  <- here("documentation")
-  
-# Run selected scripts ---------------------------------------------------------
-  
-  ## Import raw data into R format --------------------------------------------- 
-  if (IMPORT) {
-    
-    # Import survey data
-    # Requires: "DATA_BOX/encrypted/survey.csv"
-    # Creates:  "DATA_BOX/encrypted/survey.rds"
-    source(here(CODE, "import", "import-survey.R"))
-    
-  }
-  
-  ## Remove direct identifiers from data --------------------------------------- 
-  if (DEIDENTIFY) {
-    
-    # Import survey data
-    # Requires: "DATA_BOX/encrypted/survey.rds"
-    # Creates:  "DATA_BOX/deidentified/survey-deidentified.rds"
-    source(here(CODE, "deidentify", "deidentify-survey.R"))
-    
-  }
-  
-  ## Normalize units of observation -------------------------------------------- 
-  if (TIDY) {
-    
-    # Tidy child survey data
-    # Requires: "DATA_BOX/deidentified/survey-deidentified.rds"
-    # Creates:  "DATA_BOX/tidy/survey-child-tidy.rds"
-    source(here(CODE, "tidy", "tidy-child-survey.R"))
-    
-    # Tidy household survey data
-    # Requires: "DATA_BOX/deidentified/survey-deidentified.rds"
-    # Creates:  "DATA_BOX/tidy/survey-household-tidy.rds"
-    source(here(CODE, "tidy", "tidy-household-survey.R"))
-  }
-  
-  ## Optimize data format ------------------------------------------------------ 
-  if (CLEAN) {
-    
-    # Clean child survey data
-    # Requires: "DATA_BOX/tidy/survey-child-tidy.rds"
-    # Creates:  "DATA_BOX/clean/survey-child-clean.rds"
-    source(here(CODE, "clean", "clean-child-survey.R"))
-    
-    # Clean household survey data
-    # Requires: "DATA_BOX/tidy/survey-household-tidy.rds"
-    # Creates:  "DATA_BOX/clean/survey-household-clean.rds"
-    source(here(CODE, "clean", "clean-household-survey.R"))
-  }
-  
-  ## Create analysis data sets ------------------------------------------------ 
-  if (CONSTRUCT) {
-    
-    # Construct education outcomes
-    # Requires: "DATA_BOX/clean/survey-child-rds"
-    # Creates:  "DATA_BOX/constructed/child-education-constructed.rds"
-    source(here(CODE, "construct", "construct-education.R"))
-    
-    # Construct household demographics
-    # Requires: "DATA_BOX/constructed/survey-household-tidy.rds"
-    # Creates:  "DATA_BOX/constructed/household-demo-constructed.rds"
-    source(here(CODE, "construct", "construct-demo.R"))
-    
-    # Create child-level analysis data
-    # Requires: "DATA_BOX/constructed/household-demo-constructed.rds"
-    #           "DATA_BOX/constructed/child-education-constructed.rds"
-    # Creates:  "DATA_BOX/analysis/child.rds"
-    source(here(CODE, "construct", "combine-child-data.R"))
-  }
-  
-  ## Analyse data -------------------------------------------------------------- 
-  if (ANALYZE) {
-    
-    # Balance table
-    # Requires: "DATA_BOX/analysis/child.dta"
-    # Creates:  "DATA_BOX/balance-table.tex"
-    source(here(CODE, "analysis", "balance-table.R"))
-  }
-  
+gdp_pc <-
+  read_csv(
+    here(
+      "data",
+      "global",
+      "gdp_pc.csv"
+    )
+  )
+
+gdp_pc_long <-
+  gdp_pc %>%
+  pivot_longer(
+    cols = -year,
+    values_to = "gdp_pc",
+    names_to = "country"
+  )
+
+
+# Functions --------------------------------------------------------------------
+
+nominal_cost_to_op <- function(reach, unit_cost, gdp_pc) {
+  reach * log(1 - unit_cost/gdp_pc)/global_parameters$op_unit
+}
+
+nominal_to_real <- function(nominal, year) {
+  nominal / ((1 + global_parameters$discount_rate) ^ year)
+}
+
+innovation_costs <- function(data) {
+  data %>%
+    full_join(years) %>%
+    arrange(year) %>%
+    mutate(
+      div_year = year - 2010
+    ) %>%
+    mutate(
+      across(
+        contains("investment"),
+        ~ replace_na(., 0)
+      ),
+      across(
+        contains("investment"),
+        ~ . / ((1 + global_parameters$discount_rate) ^ div_year),
+        .names = "{.col}_real"
+      ),
+      across(
+        contains("investment"),
+        ~ cumsum(.),
+        .names = "{.col}_todate"
+      ),
+      total_investment_todate = div_investment_todate + nondiv_investment_todate,
+      total_investment_real_todate = div_investment_real_todate + nondiv_investment_real_todate,
+      div_share = div_investment_real_todate/total_investment_real_todate
+    )
+}
+
+net_benefits_inputs <- function(benefits_data, op_cost_data, in_cost_data) {
+  in_cost_data %>%
+    left_join(op_cost_data) %>%
+    left_join(benefits_data) %>%
+    select(
+      year, div_year, 
+      benefits_usd, op_cost_usd, 
+      div_share, div_investment_real_todate, 
+      benefits_op, op_cost_op
+    ) %>%
+    mutate(
+      net_benefits_usd = benefits_usd - op_cost_usd,
+      net_benefits_op = benefits_op + op_cost_op,
+      across(
+        starts_with("net_benefits"),
+        ~ . * div_share,
+        .names = "attributed_{.col}"
+      ),
+      across(
+        starts_with("attributed_"),
+        ~ nominal_to_real(., div_year),
+        .names = "real_{.col}"
+      ),
+      across(
+        starts_with("real_attributed"),
+        ~ cumsum(.),
+        .names = "{.col}_todate"
+      ),
+      across(
+        starts_with("real_attributed"),
+        ~ cumsum(.),
+        .names = "{.col}_todate"
+      ),
+      across(
+        ends_with("_todate"),
+        ~ . / (div_investment_real_todate * 1.12),
+        .names = "{.col}_todate"
+      )
+    )
+}
+
+
 #==============================================================================#
